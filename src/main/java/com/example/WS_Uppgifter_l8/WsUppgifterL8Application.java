13e69e4151e5d1c833fe193d51e6d96416fdd36b
Amanda Grip <amanda.grip@stud.sti.se>
2025-11-13 13:34:51 +0100

    created entity student class
    with constructor and getter and setters

---
+++ src/main/java/com/example/WS_Uppgifter_l8/WsUppgifterL8Application.java
@@ -0,0 +1,13 @@
+package com.example.WS_Uppgifter_l8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsUppgifterL8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsUppgifterL8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WS_Uppgifter_l8/WsUppgifterL8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WS_Uppgifter_l8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsUppgifterL8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
